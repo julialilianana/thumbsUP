3c61a287875d024a96f2538609caefd6ad32265a
Julia Collins <jlilianacollins@gmail.com>
2020-11-24 09:56:13 -0800

    first commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import SearchBar from './SearchBar'
+
+class App extends Component{
+  render(){
+    return (
+      <React.Fragment>
+        <h1>App here</h1>
+
+        <SearchBar/>
+      </React.Fragment>
+      
+    )
+  }
+}
+
+export default App
